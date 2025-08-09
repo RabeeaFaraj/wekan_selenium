3964b311f1eab476757710b219b0e85b2e578ecb
rabeeafaraj <rabeea.30.03@gmail.com>
2025-08-09 15:58:08 +0300

    update_selenium

--- tests/login_test.py
+++ tests/login_test.py
@@ -7,27 +7,32 @@ import os
 
 class LoginTest(unittest.TestCase):
     def setUp(self):
+       
+        # chrome_options = Options()
+        # chrome_options.add_argument("--start-maximized")  # avoids maximize_window bug
+       
+        # self.driver = webdriver.Chrome(options=chrome_options)
+
+        # WEKAN_URL = os.getenv("WEKAN_URL", "http://localhost:80")
+        # self.driver.get(f"{WEKAN_URL}/sign-in")
+        # self.login_page = LoginPage(self.driver)
         chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--headless=new")  # מריץ בלי GUI
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument("--user-data-dir=/tmp/chrome-profile")  # פרופיל זמני
+        chrome_options.add_argument("--start-maximized")  # avoids maximize_window bug
+        # chrome_options.add_argument("--headless=new")  # if you want headless
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
 
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.maximize_window()
-        # at the top of your file
-        WEKAN_URL = os.getenv("WEKAN_URL", "http://localhost:80")
 
-        # in setUp()
+        WEKAN_URL = os.getenv("WEKAN_URL", "http://localhost:80")
         self.driver.get(f"{WEKAN_URL}/sign-in")
-        #self.driver.get("http://localhost:80/sign-in")
         self.login_page = LoginPage(self.driver)
+
 
     def test_valid_login(self):
         home = self.login_page.login_as_valid_user("Rabeea.30.03@gmail.com", "30fnhk03")
         self.assertTrue(home.is_logged_in_successfully())
+        
 
     def tearDown(self):
         self.driver.quit()
