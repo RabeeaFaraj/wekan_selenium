a3b54ba0bbba6eac050d17aa6c4aaf1729869b6a
rabeeafaraj <rabeea.30.03@gmail.com>
2025-08-12 14:22:28 +0300

    aloncheckness

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.board_page import BoardPage
+import time
 
 class HomePage:
     def __init__(self, driver):
@@ -13,9 +14,14 @@ class HomePage:
     def add_board(self, board_name):
         # Example method to add a board
         # Update selector as needed for your app
+        time.sleep(10)
         self.driver.find_element(*self.board).click() 
         self.driver.find_element(*self.name_board).send_keys(board_name)
         self.driver.find_element(*self.create_button).click()
+
+        # WebDriverWait(self.driver, 50).until(
+        #     EC.presence_of_element_located((By.ID, "header-main-bar"))
+        # )
         return BoardPage(self.driver)
         # WebDriverWait(self.driver, 100).until(
         #     EC.presence_of_element_located((By.XPATH, f"//h1[text()='{board_name}']"))
