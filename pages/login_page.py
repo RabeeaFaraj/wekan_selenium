c4c7cd7e8c4e38f3bdedbeaa578212136fb296be
rabeeafaraj <rabeea.30.03@gmail.com>
2025-08-12 16:32:23 +0300

    remove wait from login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,9 +10,9 @@ class LoginPage:
         self.email_field = (By.ID, "at-field-username_and_email")
         self.password_field = (By.ID, "at-field-password")
         self.login_button = (By.ID, "at-btn")  
-        WebDriverWait(self.driver, 50).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "auth-dialog"))
-        )        
+        # WebDriverWait(self.driver, 50).until(
+        #     EC.presence_of_element_located((By.CLASS_NAME, "auth-dialog"))
+        # )        
         # Correct way to check for class presence
         try:
             self.driver.find_element(By.CLASS_NAME, "auth-dialog")
